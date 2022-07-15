58e15b71171882ed9a84310565774cfca88cedd2
Alexandre Jesus <adbjesus@gmail.com>
2022-07-15 15:56:20 +0100

    Receive hv bin in gen.py

--- tests/resources/hvdata/gen.py
+++ tests/resources/hvdata/gen.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 import numpy as np
+import argparse
+import pathlib
 
 def filter_nondom(points):
     ps = []
@@ -33,21 +35,21 @@ def generate_ndpoints(n, m, low, high):
 def generate_refp(m, low, high):
     return list(map(int, np.random.randint(low, high, size = m)))
 
-def get_hv(points, refp):
+def get_hv(points, refp, hvbin):
     # This assumes minimization, so we need to get the negative for ref and points
     points_stdin = '\n'.join(map(lambda p: ' '.join(map(str, p)), np.negative(points)))
     refp_arg = ' '.join(map(str, np.negative(refp)))
-    args = ["./hv-1.3-src/hv", "-r", refp_arg]
+    args = [hvbin, "-r", refp_arg]
     p = subprocess.run(args, input=points_stdin.encode(), capture_output=True)
     return int(float(p.stdout.decode().strip()))
 
 # Generate non-dominated points, compute its hypervolume, and save data
 # to a file
-def generate_to_file(n, m, seed, plow, phigh, rlow, rhigh, filename):
+def generate_to_file(n, m, seed, plow, phigh, rlow, rhigh, filename, hvbin):
     np.random.seed(seed)
     points = generate_ndpoints(n, m, plow, phigh)
     refp = generate_refp(m, rlow, rhigh)
-    hv = get_hv(points, refp)
+    hv = get_hv(points, refp, hvbin)
 
     ptostring = lambda p: ' '.join(map(str, p))
     with open(filename, 'w') as f:
@@ -61,6 +63,10 @@ def generate_to_file(n, m, seed, plow, phigh, rlow, rhigh, filename):
     print(f"Data written to {fname}.")
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('hvbin', type=pathlib.Path, help='path to hv executable')
+    args = parser.parse_args()
+
     rlow = -10
     rhigh = 0
     plow = 1
@@ -81,4 +87,4 @@ if __name__ == '__main__':
             print(n, m, phigh)
             for seed in range(10):
                 fname = os.path.join(datadir, f"hv_{n}_{m}_{seed}.dat")
-                generate_to_file(n, m, seed, plow, phigh, rlow, rhigh, fname)
+                generate_to_file(n, m, seed, plow, phigh, rlow, rhigh, fname, args.hvbin)
